046d7f20b128430d58c29541613e5a21535feb49
Alex And <alexandr_works@ukr.net>
2018-04-07 23:11:00 +0300

    add rotate effect

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
-    
+    var element = $('.out');
+    var area = $('.wrap');
+    var areaH = area.height();
+    var areaW = area.width();
+    var areaIn = $('.wrap-in');
+
+    area.on('mousemove', function(event) {
+        var x = event.clientX;
+        var y = event.clientY;
+
+        if (x < areaW / 2 && y < areaH / 2 || x > areaW / 2 && y > areaH / 2) {
+            areaIn.css({
+                transform: 'rotate(-45deg)'
+            });
+        }
+
+        if (x > areaW / 2 && y < areaH / 2 || x < areaW / 2 && y > areaH / 2) {
+            areaIn.css({
+                transform: 'rotate(45deg)'
+            });
+        }
+
+        element.css({
+            transform: 'rotateX(' + x + 'deg)'
+        });
+
+    });
 });
